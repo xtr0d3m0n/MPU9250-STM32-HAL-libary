ef4198eb3e2d38d75a2c573347115d59d66f3d29
xtr0d3m0n <uchiha.pubudu@gmail.com>
2019-11-22 11:21:16 +0530

    Initial Commit

---
+++ MPU9250_Config.h
@@ -0,0 +1,24 @@
+/*
+ * MPU9250_Config.h
+ *
+ *  Created on: Feb 28, 2019
+ *      Author: Desert
+ */
+
+#ifndef UTIL_MPU9250_CONFIG_H_
+#define UTIL_MPU9250_CONFIG_H_
+
+//#define USE_SPI 				1
+
+#ifdef USE_SPI
+	#include "spi.h"
+	#define MPU9250_SPI			hspi1
+	#define	MPU9250_CS_GPIO		MPU9250_CS_GPIO_Port
+	#define	MPU9250_CS_PIN		MPU9250_CS_Pin
+#else
+	#include "i2c.h"
+	#define _MPU9250_I2C		hi2c1
+	#define DEVICE_ADD			208‬
+#endif
+
+#endif /* UTIL_MPU9250_CONFIG_H_ */
